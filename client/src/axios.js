178c7a1858504922afd38bfbe6541336b19c3965
nguyenquoctoan160 <115980213+nguyenquoctoan160@users.noreply.github.com>
2024-04-17 07:32:48 +0700

    Update axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-const url = "localhost:8800";
-// const url = "social-api-ul9t.onrender.com";
-export const URL_OF_BACK_END = "http://" + url + "/api/";
-// export const URL_OF_BACK_END = "https://" + url + "/api/";
-export const WEBSOCKET_BACK_END = "ws://localhost:3030";
-// export const WEBSOCKET_BACK_END = "wss://social-api-ul9t.onrender.com";
+//const url = "localhost:8800";
+//export const URL_OF_BACK_END = "http://" + url + "/api/";
+//export const WEBSOCKET_BACK_END = "ws://localhost:3030";
+const url = "social-api-ul9t.onrender.com";
+export const URL_OF_BACK_END = "https://" + url + "/api/";
+export const WEBSOCKET_BACK_END = "wss://social-api-ul9t.onrender.com";
 export const makeRequest = axios.create({
   baseURL: URL_OF_BACK_END,
   withCredentials: true,
